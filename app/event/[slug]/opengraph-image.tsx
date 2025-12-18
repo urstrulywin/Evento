dd33a4c481f6a28a7787e57542add6d9decfb7fd
Vishnu <vishnudusa0607@gmail.com>
2025-12-18 22:18:53 +0530

    feat: added opengraph img

---
+++ app/event/[slug]/opengraph-image.tsx
@@ -0,0 +1,15 @@
+import { ImageResponse } from "next/og";
+
+export const alt = "Evemto";
+export const size = { width: 1200, height: 630 };
+export const contentType = "image/png";
+export default async function Image({ params }: { params: { slug: string } }) {
+    return new ImageResponse(
+      (
+        <section>
+            <h1>{params.slug}</h1>
+            <p>Evento - Browse events around you</p>
+        </section>
+        ),
+    );
+}
